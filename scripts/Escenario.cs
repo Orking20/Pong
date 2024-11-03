8f669ad1e12c07dd780f5885322a380b34b56ca6
Orking20 <mateoluchetti@hotmail.com>
2024-11-03 15:44:23 +0100

    Mover paletas dentro de los límites de la pantalla

--- scripts/Escenario.cs
+++ scripts/Escenario.cs
@@ -3,21 +3,22 @@ using System;
 
 public partial class Escenario : Node2D
 {
+	private Vector2 _tamanoPantalla;
 	private CharacterBody2D _paletaIzq;
 	private CharacterBody2D _paletaDer;
 	private int _velocidadPaleta;
 
 	public override void _Ready()
 	{
-		var tamanoPantalla = GetViewportRect().Size;
+		this._tamanoPantalla = GetViewportRect().Size;
 
 		CharacterBody2D bola = GetNode<CharacterBody2D>("Bola");
 		this._paletaIzq = GetNode<CharacterBody2D>("PaletaIzq");
 		this._paletaDer = GetNode<CharacterBody2D>("PaletaDer");
 
-		bola.Position = tamanoPantalla / 2;
-		this._paletaIzq.Position = new Vector2(20, tamanoPantalla.Y / 2);
-		this._paletaDer.Position = new Vector2(620, tamanoPantalla.Y / 2);
+		bola.Position = this._tamanoPantalla / 2;
+		this._paletaIzq.Position = new Vector2(20, this._tamanoPantalla.Y / 2);
+		this._paletaDer.Position = new Vector2(620, this._tamanoPantalla.Y / 2);
 
 		this._velocidadPaleta = 150;
 	}
@@ -37,20 +38,21 @@ public partial class Escenario : Node2D
 		this._paletaIzq.Velocity = Vector2.Zero;
 		this._paletaDer.Velocity = Vector2.Zero;
 
-		if (Input.IsActionPressed("izq_mover_arriba"))
+		// Mueve las paletas dentro de los límites de la pantalla
+		if (Input.IsActionPressed("izq_mover_arriba") && this._paletaIzq.Position >= new Vector2(20, 0))
 		{
 			this._paletaIzq.Velocity -= new Vector2(0, this._velocidadPaleta);
 		}
-		else if (Input.IsActionPressed("izq_mover_abajo"))
+		else if (Input.IsActionPressed("izq_mover_abajo") && this._paletaIzq.Position <= new Vector2(20, this._tamanoPantalla.Y))
 		{
 			this._paletaIzq.Velocity += new Vector2(0, this._velocidadPaleta);
 		}
-		
-		if (Input.IsActionPressed("der_mover_arriba"))
+
+		if (Input.IsActionPressed("der_mover_arriba") && this._paletaDer.Position >= new Vector2(620, 0))
 		{
 			this._paletaDer.Velocity -= new Vector2(0, this._velocidadPaleta);
 		}
-		else if (Input.IsActionPressed("der_mover_abajo"))
+		else if (Input.IsActionPressed("der_mover_abajo") && this._paletaDer.Position <= new Vector2(620, this._tamanoPantalla.Y))
 		{
 			this._paletaDer.Velocity += new Vector2(0, this._velocidadPaleta);
 		}
